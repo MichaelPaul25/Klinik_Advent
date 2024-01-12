aa246a36ecd8a7c9ecd986f88c0a30eca02f0972
Michael <wariorstore17@gmail.com>
2024-01-12 16:51:42 +0700

    Update Pendaftaran page

---
+++ Website_Klinik/Controllers/PendaftaranController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website_Klinik.Controllers
+{
+    public class PendaftaranController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
