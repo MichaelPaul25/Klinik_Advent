968fedfe859ff54d440805d357d38e1fe02538a6
Michael <wariorstore17@gmail.com>
2024-01-15 22:08:35 +0700

    Update
    - Add Models TBL_PENDAFTARAN
    - Update library
    - Add Script
    - Add View pendaftaran

---
+++ Website_Klinik/Data/Enum/Kategori_Tindakan_Gigi.cs
@@ -0,0 +1,9 @@
+﻿namespace Website_Klinik.Data.Enum
+{
+    public enum Kategori_Tindakan_Gigi
+    {
+        Scalling,
+        Cabut,
+        Tambal
+    }
+}
---
+++ Website_Klinik/Data/Enum/Kategori_Tindakan_Umum.cs
@@ -0,0 +1,8 @@
+﻿namespace Website_Klinik.Data.Enum
+{
+    public enum Kategori_Tindakan_Umum
+    {
+        Menjahit,
+        Suntik
+    }
+}
